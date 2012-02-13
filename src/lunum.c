jzrake/lunum

commit f2e21b319bd90494cc19f2694dcd03680c1b99a0

Incremental commit toward building slices.

src/lunum.c
@@ -295,6 +295,31 @@ int luaC_array__call(lua_State *L)
 int luaC_array__index(lua_State *L)
 {
   struct Array *A = lunum_checkarray1(L, 1);
+
+  // Figure out what is the format of the input index. If it's a number or a
+  // table of numbers, then pass it along to _get_index. If it's a table of
+  // tables or numbers, then assume it's a slice.
+  // ---------------------------------------------------------------------------
+  int slice = 0;
+  if (lua_istable(L, 2)) {
+
+    lua_getglobal(L, "lunum");
+    lua_getfield(L, -1, "__build_slice");
+    lua_pushvalue(L, 2);
+    lua_call(L, 1, 0);
+    lua_pop(L, 1);
+
+    int nind = lua_rawlen(L, 2);
+
+    for (int n=1; n<=nind; ++n) {
+      lua_rawgeti(L, 2, n);
+      slice += lua_istable(L, -1);
+      lua_pop(L, 1);
+    }
+  }
+
+  return 0;
+
   const int m = _get_index(L, A);
 
   switch (A->dtype) {

test/slice.lua
@@ -4,6 +4,7 @@ require 'lunum'
 
 local A = lunum.range(50)
 
+print(A[{{0,10,2},{0,6},12}])
 print(A)
 
 print(lunum.slice(A,0,10,1))

src/array_class.lua
@@ -220,12 +220,31 @@ local function apply(f,...)
    return B
 end
 
+local function __build_slice(s)
+
+   slice = { }
+   print("checking slice...", #s)
+
+   for k,v in pairs(s) do
+      if type(v) == 'number' then
+	 slice[k] = {v,-1,1}
+      elseif type(v) == 'table' and #v == 2 then
+	 slice[k] = {v[1], v[2], 1}
+      elseif type(v) == 'table' and #v == 3 then
+	 slice[k] = {v[1], v[2], v[3]}
+      end
+   end
+   for k,v in pairs(slice) do
+      print(lunum.array(v))
+   end
+
+end
 
 -- -----------------------------------------------------------------------------
 -- This function gets called from C code to register an array's class methods
 -- have been were implemented in Lua.
 -- -----------------------------------------------------------------------------
-local function __register(t)
+local function __register_array(t)
    t.copy      = copy
    t.min       = min
    t.max       = max
@@ -248,5 +267,7 @@ end
 -- -----------------------------------------------------------------------------
 -- Registering the functions with the lunum table.
 -- -----------------------------------------------------------------------------
-lunum.__register_array = __register
+lunum.__build_slice    = __build_slice
+lunum.__register_array = __register_array
 lunum.apply = apply
+

commit 35381dad7528d2bb0d98966146007a5ed1788e6a

Fixed a bug introduced back in f2e21b319bd90494cc19f2694dcd03680c1b99a0 where A[i] type indices were being passed to the slicing function; must use lua_type, not lua_isstring. Also fixed an arithmetic bug in the numarray function array_new_from_slice, computing strides along higher dimensions incorrectly.

src/lunum.c
@@ -97,7 +97,9 @@ static int _complex_binary_op2(lua_State *L, enum ArrayOperation op);
 
 static void _unary_func(lua_State *L, double(*f)(double), Complex(*g)(Complex),
 			int cast);
+static void _push_value(lua_State *L, enum ArrayType T, void *v);
 static int _get_index(lua_State *L, struct Array *A);
+
 
 static Complex ImaginaryUnit = I;
 
@@ -275,18 +277,14 @@ int luaC_array__call(lua_State *L)
   for (int d=Nd-2; d>=0; --d) {
     stride[d] = stride[d+1] * A->shape[d+1];
   }
-  int m = 0;
 
+  int m = 0;
   for (int d=0; d<A->ndims; ++d) {
     int i = lua_tointeger(L, d+2);
     m += i*stride[d];
   }
 
-  luaL_getmetafield(L, 1, "__index");
-  lua_pushvalue(L, 1);
-  lua_pushnumber(L, m);
-
-  lua_call(L, 2, 1);
+  _push_value(L, A->dtype, (char*)A->data + m*array_sizeof(A->dtype));
   free(stride);
 
   return 1;
@@ -301,7 +299,7 @@ int luaC_array__index(lua_State *L)
   // tables or numbers, then assume it's a slice.
   // ---------------------------------------------------------------------------
 
-  if (lua_istable(L, 2) || lua_isstring(L, 2)) {
+  if (lua_type(L, 2) == LUA_TTABLE || lua_type(L, 2) == LUA_TSTRING) {
 
     lua_getglobal(L, "lunum");
     lua_getfield(L, -1, "__build_slice");
@@ -314,17 +312,7 @@ int luaC_array__index(lua_State *L)
   }
 
   const int m = _get_index(L, A);
-
-  switch (A->dtype) {
-  case ARRAY_TYPE_BOOL    : lua_pushboolean(L,    ((Bool   *)A->data)[m]); break;
-  case ARRAY_TYPE_CHAR    : lua_pushnumber (L,    ((char   *)A->data)[m]); break;
-  case ARRAY_TYPE_SHORT   : lua_pushnumber (L,    ((short  *)A->data)[m]); break;
-  case ARRAY_TYPE_INT     : lua_pushnumber (L,    ((int    *)A->data)[m]); break;
-  case ARRAY_TYPE_LONG    : lua_pushnumber (L,    ((long   *)A->data)[m]); break;
-  case ARRAY_TYPE_FLOAT   : lua_pushnumber (L,    ((float  *)A->data)[m]); break;
-  case ARRAY_TYPE_DOUBLE  : lua_pushnumber (L,    ((double *)A->data)[m]); break;
-  case ARRAY_TYPE_COMPLEX : lunum_pushcomplex (L, ((Complex*)A->data)[m]); break;
-  }
+  _push_value(L, A->dtype, (char*)A->data + array_sizeof(A->dtype)*m);
 
   return 1;
 }
@@ -584,6 +572,7 @@ int luaC_lunum_slice(lua_State *L)
   int *skip    = (int*) lunum_checkarray2(L, 4, ARRAY_TYPE_INT, &Nd2);
   int *squeeze = (int*) lunum_checkarray2(L, 5, ARRAY_TYPE_INT, &Nd3);
 
+
   if (Nd0 != A->ndims || Nd1 != A->ndims || Nd2 != A->ndims || Nd3 != A->ndims) {
     luaL_error(L, "slice has wrong number of dimensions for array");
   }
@@ -602,20 +591,26 @@ int luaC_lunum_slice(lua_State *L)
   int Nd_new = 0;
   for (int d=0; d<Nd0; ++d) Nd_new += !squeeze[d];
 
-  int *shape_new = (int*) malloc(Nd_new * sizeof(int));
+  // In case we're left with a 0-dimensional (scalar) slice
+  if (Nd_new == 0) {
+    _push_value(L, B.dtype, B.data);
+    return 1;
+  }
+  // In case there are any dims to squeeze out
+  else if (Nd_new != Nd0) {
 
-  for (int d=0,e=0; d<Nd0; ++d) {
-    if (B.shape[d] > 1 || !squeeze[d]) {
-      shape_new[e] = B.shape[d];
-      ++e;
+    int *shape_new = (int*) malloc(Nd_new * sizeof(int));
+    for (int d=0,e=0; d<Nd0; ++d) {
+      if (B.shape[d] > 1 || !squeeze[d]) {
+	shape_new[e] = B.shape[d];
+	++e;
+      }
     }
+    array_resize(&B, shape_new, Nd_new);
+    free(shape_new);
   }
 
-  array_resize(&B, shape_new, Nd_new);
-  free(shape_new);
-
   lunum_pusharray1(L, &B);
-
   return 1;
 }
 
@@ -736,3 +731,19 @@ int _get_index(lua_State *L, struct Array *A)
   }
   return m;
 }
+
+
+void _push_value(lua_State *L, enum ArrayType T, void *v)
+{
+  switch (T) {
+  case ARRAY_TYPE_BOOL    : lua_pushboolean(L,    *((Bool   *)v)); break;
+  case ARRAY_TYPE_CHAR    : lua_pushnumber (L,    *((char   *)v)); break;
+  case ARRAY_TYPE_SHORT   : lua_pushnumber (L,    *((short  *)v)); break;
+  case ARRAY_TYPE_INT     : lua_pushnumber (L,    *((int    *)v)); break;
+  case ARRAY_TYPE_LONG    : lua_pushnumber (L,    *((long   *)v)); break;
+  case ARRAY_TYPE_FLOAT   : lua_pushnumber (L,    *((float  *)v)); break;
+  case ARRAY_TYPE_DOUBLE  : lua_pushnumber (L,    *((double *)v)); break;
+  case ARRAY_TYPE_COMPLEX : lunum_pushcomplex (L, *((Complex*)v)); break;
+  }
+}
+

src/numarray.c
@@ -318,7 +318,6 @@ void array_assign_from_array(struct Array *A, const struct Array *B)
   }
 }
 
-
 struct Array array_new_from_slice(const struct Array *B1,
 				  int *start, int *stop, int *skip, int Nd)
 // -----------------------------------------------------------------------------
@@ -360,9 +359,9 @@ struct Array array_new_from_slice(const struct Array *B1,
   while (J[0] < N[0]) {
 
     int M = 0;
-    for (int d=0; d<Nd; ++d) M += J[d] * S[d] * skip[d] + start[d];
+    for (int d=0; d<Nd; ++d) M += (J[d] * skip[d] + start[d]) * S[d];
 
-    // ----- use the index x -----
+    // ----- use the indices m,M -----
     memcpy(b0 + (m++)*sizeof_T, b1 + M*sizeof_T, sizeof_T);
     // -----                 -----
 

test/slice.lua
@@ -5,24 +5,28 @@ require 'lunum'
 local A = lunum.range(200)
 local B = A:reshape{10,10,2}
 
+if true then
+   print("testing manual slicing function")
+   print(lunum.slice(A,0,10,1,0))
+   print(lunum.slice(A,0,10,2,0))
+   print(lunum.slice(A,0,10,3,0))
+   print(lunum.slice(A,0,10,4,0))
 
-print("testing manual slicing function")
-print(lunum.slice(A,0,10,1,0))
-print(lunum.slice(A,0,10,2,0))
-print(lunum.slice(A,0,10,3,0))
-print(lunum.slice(A,0,10,4,0))
+   print("testing squeeze...")
+   print("[ 5, 5, 1 ] ?= ", lunum.slice(B, {0,0,0}, {10,10,2}, {2,2,2}, {0,0,0}):shape('array'))
+   print("[ 5, 5 ]    ?= ", lunum.slice(B, {0,0,0}, {10,10,2}, {2,2,2}, {0,0,1}):shape('array'))
 
-print("testing squeeze...")
-print("[ 5, 5, 1 ] ?= ", lunum.slice(B, {0,0,0}, {10,10,2}, {2,2,2}, {0,0,0}):shape('array'))
-print("[ 5, 5 ]    ?= ", lunum.slice(B, {0,0,0}, {10,10,2}, {2,2,2}, {0,0,1}):shape('array'))
+   print("testing slicing with tables...")
+   print("[ 0, 4 ]    ?= ", A[{{0,8,4}}])
+   print("[ 2, 2, 2 ] ?= ", B[{{0,10,5}, {0,10,5}, {0,2,1}}]:shape('array'))
+   print("[ 2, 2 ]    ?= ", B[{{0,10,5}, {0,10,5}, 0}]:shape('array'))
 
-print("testing slicing with tables...")
-print("[ 0, 4 ]    ?= ", A[{{0,8,4}}])
-print("[ 2, 2, 2 ] ?= ", B[{{0,10,5}, {0,10,5}, {0,2,1}}]:shape('array'))
-print("[ 2, 2 ]    ?= ", B[{{0,10,5}, {0,10,5}, 0}]:shape('array'))
+   print("testing slicing with strings...")
+   print("[ 0, 4 ]    ?= ", A['0:8:4'])
+   print("[ 2, 2, 2 ] ?= ", B['0:10:5,0:10:5,0:2:1']:shape('array'))
+   print("[ 2, 2 ]    ?= ", B['0:10:5,0:10:5, 0 '  ]:shape('array'))
+end
 
-print("testing slicing with strings...")
-print("[ 0, 4 ]    ?= ", A['0:8:4'])
-print("[ 2, 2, 2 ] ?= ", B['0:10:5,0:10:5,0:2:1']:shape('array'))
-print("[ 2, 2 ]    ?= ", B['0:10:5,0:10:5, 0 '  ]:shape('array'))
-
+print("         45 ?= ", B['2,2,1'])
+print("         45 ?= ", B[{2,2,1}])
+print("         45 ?= ", B( 2,2,1 ))